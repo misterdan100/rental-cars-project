d61e18f87290d410a23d14853ba41060e30aeb52
misterdan100 <danielmca60@gmail.com>
2024-07-28 10:16:45 -0500

    core: create sidebar menu links

--- app/(routes)/(dashboard)/dashboard/components/LogoDashboard/LogoDashboard.tsx
+++ app/(routes)/(dashboard)/dashboard/components/LogoDashboard/LogoDashboard.tsx
@@ -5,7 +5,7 @@ export function LogoDashboard() {
   return (
     <Link 
       href='/'
-      className="flex items-center h-20 gap-2 border-b cursor-pointer min-h-20"
+      className="flex items-center px-6 h-20 gap-2 border-b cursor-pointer min-h-20"
     >
       <Image src='./logo.svg' alt="Logo" width={30} height={30} priority/>
       <h1 className="text-xl font-bold">Mister Cars</h1>
