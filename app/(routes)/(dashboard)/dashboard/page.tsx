7c1c34a001692585eefc9b8ba7d82810a8cc1e7c
misterdan100 <danielmca60@gmail.com>
2024-08-12 23:01:07 -0500

    core: install prisma, model db Car and Order, and generate them

--- app/(routes)/(dashboard)/dashboard/page.tsx
+++ app/(routes)/(dashboard)/dashboard/page.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function DashboardPage() {
   return (
-    <div>DashboardPage</div>
+    <div>
+      <div className="flex justify-between">
+        <h2 className='text-2xl font-bold'>List of cars</h2>
+      </div>
+      <p>Listado de coches</p>
+    </div>
   )
 }
